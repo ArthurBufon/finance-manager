2ab8924bb49672e3b27339b86feb90b00cc96909
arthur <arthurbufon2052@gmail.com>
2025-02-20 20:29:30 -0300

    Resource controllers for each resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GoalController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +27,21 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Accounts
+    Route::resource('accounts', AccountController::class);
+
+    // Transactions
+    Route::resource('accounts', TransactionController::class);
+
+    // Goals
+    Route::resource('goals', GoalController::class);
+
+    // Categories
+    Route::resource('categories', CategoryController::class);
+
+    // Users
+    Route::resource('users', UserController::class);
 });
 
 require __DIR__.'/auth.php';
